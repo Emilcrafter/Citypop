c5541aa5f494da0fc58792b6662a99e407eadef6
Emil Franzell <efranz@kth.se>
2020-12-12 08:08:44 -0500

    Fetch working for a set city name

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import {Text, View, Button, TextInput } from 'react-native';
+import {Text, View, Button, TextInput , StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { FlatList } from 'react-native-gesture-handler';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const SearchBar = () => {
   const [value, onChangeText] = React.useState('');
@@ -29,44 +32,69 @@ function HomeScreen({ navigation }){
   )
 }
 
+
+
+/* 
 const CityListFromCountry = async () => {
   try{
     let response = await fetch(
       'http://api.geonames.org/searchJSON?q=london&username=weknowit'
     );
     let json = await response.json();
-    let result = [json.geonames[0].name, json.geonames[0].population];
-    return result;
+    var items = [];
+    console.log(Object.keys(json.geonames).length);
+    for(var i = 0; i < Object.keys(json.geonames).length; i++){
+      items[i] = <li>json.geonames[i].name</li>;
+    }
+    console.log(items[0]);
+    return items;
   } catch(error){
     console.error(error);
     console.log('bruh');
   }
 }
+*/
+///console.log(<CityListFromCountry></CityListFromCountry>)
 
-console.log(CityListFromCountry())
+
+
 
 function CitiesScreen({ route, navigation }){
+const [cityList, setCityList] = useState([]);  
+  useEffect(()=> {
+    fetch(
+      'http://api.geonames.org/searchJSON?q=london&username=weknowit')
+      .then((response) => response.json())
+      .then((json) => setCityList(json.geonames))
+      .catch((error) => console.error(error))
+    }, []);
   return(
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <SearchBar/>
       <Text>Cities Screen</Text>
+      <FlatList 
+      data = {cityList}
+      renderItem={({item}) => (
+        <Text>{item.name}</Text>
+      )} 
+      contentContainerStyle = {styles.container}
+      keyExtractor = {(item) => item.geonameID}
+      />
+      
       <Button
-  title="Go to Details... again"
-  onPress={() => navigation.push('Cities')}
+  title="Go to City list"
+  onPress={() => navigation.push('CityPop')}
 />
     </View>
   )
 }
 
+  
+
 function CityPop({ route, navigation }){
   return(
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <SearchBar/>
-      <Text>Cities Screen</Text>
-      <Button
-  title="Go to Details... again"
-  onPress={() => navigation.push('Cities')}
-/>
+
     </View>
   )
 }
@@ -87,10 +115,19 @@ export default function App() {
         component = {CitiesScreen}  
         options = {{title: "Search by city name"}}
         />
+        <Stack.Screen 
+        name = "CityPop" 
+        component = {CityPop}  
+        options = {{title: "Population"}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container : {
+    paddingTop: 30
+  }
+})
 
