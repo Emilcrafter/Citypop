9d2edcc95a8f91a562c32097dde3795dc2a82cda
Emil Franzell <efranz@kth.se>
2020-12-12 16:25:22 -0500

    City List completed from user input

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import {Text, View, Button, TextInput , StyleSheet} from 'react-native';
+import {Text, View, Button, TextInput , StyleSheet, Pressable} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { FlatList } from 'react-native-gesture-handler';
+import { FlatList, TouchableHighlight } from 'react-native-gesture-handler';
 import { useEffect } from 'react';
 import { useState } from 'react';
 
-const SearchBar = () => {
-  const [value, onChangeText] = React.useState('');
 
-  return(
-    <TextInput
-    style={{ width:300, height: 50, borderColor: 'gray', borderWidth: 1}}
-    onChangeText={text => onChangeText(text)}
-    value={value}
-    />
-  )
-}
+
 
 function HomeScreen({ navigation }){
   return(
@@ -26,7 +17,26 @@ function HomeScreen({ navigation }){
       <Text>Home Screen</Text>
       <Button 
       title="Search by city"
-      onPress={() => navigation.navigate('Cities')}
+      onPress={() => navigation.navigate('CitySearch')}
+      />
+    </View>
+  )
+}
+
+function CitySearch({ navigation }){
+  const [textInputValue, onChangeText] = React.useState('');
+  return(
+    <View style={{flex : 1, alignItems: 'center', justifyContent: 'center'}}>
+      <StatusBar></StatusBar>
+      <Text>Home Screen</Text>
+      <TextInput
+        style={{ width:300, height: 50, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={text => onChangeText(text)}
+        textInputValue={textInputValue}
+    />
+      <Button 
+      title="Search by city"
+      onPress={() => navigation.navigate('CitySearchResult', {textInputValue})}
       />
     </View>
   )
@@ -34,67 +44,67 @@ function HomeScreen({ navigation }){
 
 
 
-/* 
-const CityListFromCountry = async () => {
-  try{
-    let response = await fetch(
-      'http://api.geonames.org/searchJSON?q=london&username=weknowit'
-    );
-    let json = await response.json();
-    var items = [];
-    console.log(Object.keys(json.geonames).length);
-    for(var i = 0; i < Object.keys(json.geonames).length; i++){
-      items[i] = <li>json.geonames[i].name</li>;
-    }
-    console.log(items[0]);
-    return items;
-  } catch(error){
-    console.error(error);
-    console.log('bruh');
-  }
-}
-*/
-///console.log(<CityListFromCountry></CityListFromCountry>)
 
-
-
-
-function CitiesScreen({ route, navigation }){
+function CitySearchResult({ route, navigation }){
 const [cityList, setCityList] = useState([]);  
   useEffect(()=> {
+    var q = route.params.textInputValue;
+    console.log(q);
     fetch(
-      'http://api.geonames.org/searchJSON?q=london&username=weknowit')
+      'http://api.geonames.org/searchJSON?username=weknowit&cities=cities1000&q=' + q)
       .then((response) => response.json())
       .then((json) => setCityList(json.geonames))
       .catch((error) => console.error(error))
     }, []);
   return(
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <SearchBar/>
+      
       <Text>Cities Screen</Text>
+      
       <FlatList 
       data = {cityList}
       renderItem={({item}) => (
-        <Text>{item.name}</Text>
+        
+        <TouchableHighlight onPress = {() => navigation.navigate( 
+          "CityPop",
+        {
+          key: item.geonameID,
+          cityName: item.name,
+          population: item.population, 
+        }
+        
+        )}>
+          <View
+          style = {{
+            height: 50,
+            backgroundColor: "aliceblue"
+          }}
+          
+          >
+          <Text style ={{ 
+            fontSize: 20,
+            marginRight: 150,
+          }}>{(item.name)}</Text>
+          </View>
+        </TouchableHighlight>
       )} 
       contentContainerStyle = {styles.container}
       keyExtractor = {(item) => item.geonameID}
       />
-      
-      <Button
-  title="Go to City list"
-  onPress={() => navigation.push('CityPop')}
-/>
     </View>
   )
 }
 
-  
+
 
 function CityPop({ route, navigation }){
+  console.log(route)
   return(
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-
+    <View style={{flex : 1, alignItems: 'center', justifyContent: 'center'}}>
+      <StatusBar></StatusBar>
+      <Text style = {styles.text}>{route.params.cityName}</Text>
+      <Text style = {styles.text}>Population</Text>
+      <Text style = {styles.text}>{route.params.population}</Text>
     </View>
   )
 }
@@ -111,9 +121,14 @@ export default function App() {
          options = {{ title: 'Overview'}}
          />
         <Stack.Screen 
-        name = "Cities" 
-        component = {CitiesScreen}  
+        name = "CitySearch" 
+        component = {CitySearch}  
         options = {{title: "Search by city name"}}
+        />
+        <Stack.Screen 
+        name = "CitySearchResult" 
+        component = {CitySearchResult}  
+        options = {{title: "Search results"}}
         />
         <Stack.Screen 
         name = "CityPop" 
@@ -128,6 +143,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container : {
     paddingTop: 30
+  },
+  text : {
+    fontSize : 20
   }
 })
 
