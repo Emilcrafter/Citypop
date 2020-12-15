aad126ce3662069f929691d6189dff41d650a2d5
Emil Franzell <efranz@kth.se>
2020-12-15 13:23:50 -0500

    Added country search with restrictions

--- App.js
+++ App.js
@@ -19,6 +19,10 @@ function HomeScreen({ navigation }){
       title="Search by city"
       onPress={() => navigation.navigate('CitySearch')}
       />
+            <Button 
+      title="Search by country"
+      onPress={() => navigation.navigate('CountrySearch')}
+      />
     </View>
   )
 }
@@ -33,10 +37,30 @@ function CitySearch({ navigation }){
         style={{ width:300, height: 50, borderColor: 'gray', borderWidth: 1}}
         onChangeText={text => onChangeText(text)}
         textInputValue={textInputValue}
+        onSubmitEditing={() => navigation.navigate('CitySearchResult', {textInputValue})}
     />
       <Button 
       title="Search by city"
       onPress={() => navigation.navigate('CitySearchResult', {textInputValue})}
+      />
+    </View>
+  )
+}
+
+function CountrySearch({ navigation }){
+  const [textInputValue, onChangeText] = React.useState('');
+  return(
+    <View style={{flex : 1, alignItems: 'center', justifyContent: 'center'}}>
+      <StatusBar></StatusBar>
+      <Text>Home Screen</Text>
+      <TextInput
+        style={{ width:300, height: 50, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={text => onChangeText(text)}
+        textInputValue={textInputValue}
+    />
+      <Button 
+      title="Search by country"
+      onPress={() => navigation.navigate('Country', {textInputValue})}
       />
     </View>
   )
@@ -47,6 +71,7 @@ function CitySearch({ navigation }){
 
 function CitySearchResult({ route, navigation }){
 const [cityList, setCityList] = useState([]);  
+var q = route.params.textInputValue.trim();
   useEffect(()=> {
     var q = route.params.textInputValue;
     console.log(q);
@@ -56,6 +81,9 @@ const [cityList, setCityList] = useState([]);
       .then((json) => setCityList(json.geonames))
       .catch((error) => console.error(error))
     }, []);
+    const firstElem = cityList.map((item) => item.name)[0];
+      var valid = (q) => {return(firstElem === q)};
+  if(valid(q)){
   return(
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       
@@ -94,6 +122,21 @@ const [cityList, setCityList] = useState([]);
     </View>
   )
 }
+else{
+  return(
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text style ={{ 
+          fontSize: 20,
+        }}>{"No city with the name " + q + " was found."}</Text>
+            <Button 
+            title="Return to search"
+            onPress={() => navigation.goBack()}
+            />
+
+    </View>
+  )
+}
+}
 
 
 
@@ -108,6 +151,76 @@ function CityPop({ route, navigation }){
     </View>
   )
 }
+function Country({ route, navigation }){
+  const [cityList, setCityList] = useState([]); 
+  var q = route.params.textInputValue.trim();
+    useEffect(()=> {
+      console.log(q);
+      fetch(
+        'http://api.geonames.org/searchJSON?username=weknowit&cities=cities1000&q=' + q)
+        .then((response) => response.json())
+        .then((json) => setCityList(json.geonames))
+        .catch((error) => console.error(error))
+      }, []);
+      const firstElem = cityList.map((item) => item.countryName)[0];
+      var valid = (q) => {return(firstElem === q)};
+        console.log(valid(q))
+    if(valid(q)){
+    return(
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <FlatList 
+        data = {cityList.map((item) => item)}
+        key = {(item) => item.geonameID}
+        renderItem={({item}) => (
+          
+          <TouchableHighlight onPress = {() => navigation.navigate( 
+            "CityPop",
+          {
+            key: item.geonameID,
+            cityName: item.name,
+            population: item.population, 
+          }
+          
+          )}>
+
+            <View
+            style = {{
+              height: 50,
+              backgroundColor: "aliceblue"
+            }}
+            
+            >
+            <Text style ={{ 
+              fontSize: 20,
+              marginRight: 150,
+            }}>{(item.name)}</Text>
+            </View>
+          </TouchableHighlight>
+        )} 
+        contentContainerStyle = {styles.container}
+        keyExtractor = {(item) => item.geonameID}
+        />
+      </View>
+    )}
+    else{
+      return(
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style ={{ 
+              fontSize: 20,
+            }}>{"No country with the name " + q + " was found."}</Text>
+                <Button 
+                title="Return to search"
+                onPress={() => navigation.goBack()}
+                />
+
+        </View>
+
+
+      )
+    }
+  }
+  
+
 
 const Stack = createStackNavigator();
 
@@ -126,6 +239,11 @@ export default function App() {
         options = {{title: "Search by city name"}}
         />
         <Stack.Screen 
+        name = "CountrySearch" 
+        component = {CountrySearch}  
+        options = {{title: "Search by country name"}}
+        />
+        <Stack.Screen 
         name = "CitySearchResult" 
         component = {CitySearchResult}  
         options = {{title: "Search results"}}
@@ -134,6 +252,11 @@ export default function App() {
         name = "CityPop" 
         component = {CityPop}  
         options = {{title: "Population"}}
+        />
+        <Stack.Screen 
+        name = "Country" 
+        component = {Country}  
+        options = {({ route }) => ({title: route.params.textInputValue})}
         />
       </Stack.Navigator>
     </NavigationContainer>
